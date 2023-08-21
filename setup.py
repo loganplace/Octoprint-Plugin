09597c80e780c706e04064b85021e6aca8a8959f
Logan Place <lplace@sunsh1n3.com>
2023-08-21 14:49:38 -0400

    init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "helloworld"
+plugin_identifier = "helllllloworld"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_helloworld"
